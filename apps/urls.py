6854293cd307026c20aea24132950b5f9966257d
hossainchisty <hossain.chisty11@gmail.com>
2021-04-08 00:58:31 +0600

    add new path

--- apps/urls.py
+++ apps/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home_page),
+    path('',views.home_page, name="home"),
+    path('contact/', views.contact, name="contact"),
+    path('book_table/', views.book_table, name="book_table"),
 ]
